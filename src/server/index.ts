d5a389761f2e37edc536aeae0cccaedfb571f403
NickRC6 <nickrc6@gmail.com>
2026-06-03 21:58:11 +0700

    RabbitMQ Baseline

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,15 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection succesful!")
+
+  process.on('SIGINT', () => {
+    console.log('Shutting down...');
+    conn.close()
+  });
 }
 
 main().catch((err) => {
