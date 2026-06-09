c663b8997cf58307b49700a826d28a5a0e8c843a
NickRC6 <nickrc6@gmail.com>
2026-06-09 22:23:15 +0700

    Added durable queue

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,8 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { ExchangePerilDirect, ExchangePerilTopic, GameLogSlug, PauseKey } from "../internal/routing/routing.js";
 import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
+import { declareAndBind, SimpleQueueType } from "../internal/pubsub/declareAndBind.js";
 
 
 async function main() {
@@ -10,6 +11,7 @@ async function main() {
   const conn = await amqp.connect(rabbitConnString);
   console.log("Connection succesful!");
   const confChannel = await conn.createConfirmChannel()
+  const declareQueue = await declareAndBind(conn, ExchangePerilTopic, GameLogSlug, `${GameLogSlug}.*`, SimpleQueueType.Durable);
 
   const serverState = true
   printServerHelp()
