a030b43769bd96b3d361253565b27e79bf7d6271
NickRC6 <nickrc6@gmail.com>
2026-06-09 21:28:42 +0700

    Adding REPL

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 
 
 async function main() {
@@ -10,7 +11,37 @@ async function main() {
   console.log("Connection succesful!");
   const confChannel = await conn.createConfirmChannel()
 
-  publishJSON(confChannel, ExchangePerilDirect, PauseKey, { isPaused: true } )
+  const serverState = true
+  printServerHelp()
+
+  while (serverState) {
+    const inputArray = await getInput();
+
+    if (!inputArray) {
+      continue;
+    }
+
+    const firstWord = inputArray[0];
+
+    if (firstWord == "pause") {
+      console.log("Sending pause message...");
+      publishJSON(confChannel, ExchangePerilDirect, PauseKey, { isPaused: true } )
+    }
+
+    else if (firstWord == "resume") {
+      console.log("Sending resume message...");
+      publishJSON(confChannel, ExchangePerilDirect, PauseKey, { isPaused: false } )
+    }
+
+    else if (firstWord == "quit") {
+      console.log("Exiting...");
+      break;
+    }
+
+    else {
+      console.log("Command not found!");
+    }
+  }
 
   process.on('SIGINT', () => {
     console.log('Shutting down...');
