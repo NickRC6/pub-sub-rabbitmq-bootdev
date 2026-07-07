16287ad2f31d18e3fd7b3f7f09ed68e9cf7c7136
NickRC6 <nickrc6@gmail.com>
2026-07-07 21:15:10 +0700

    Added msgpack and publish method

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,6 @@
 import { type ConfirmChannel } from "amqplib";
+import { encode } from "@msgpack/msgpack";
+
 
 export function publishJSON<T>(
   ch: ConfirmChannel,
@@ -14,6 +16,31 @@ export function publishJSON<T>(
       routingKey,
       serial,
       { contentType: "application/json" },
+      (err: Error | null) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      }
+    );
+  });
+}
+
+export function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const serial = Buffer.from(encode(value));
+
+  return new Promise((resolve, reject) => {
+    ch.publish(
+      exchange,
+      routingKey,
+      serial,
+      { contentType: "application/x-msgpack" },
       (err: Error | null) => {
         if (err) {
           reject(err);
